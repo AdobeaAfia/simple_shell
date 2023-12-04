1d41887f0cc4574967b0813138ba15c5726f5f04
AdobeaAfia <adobea_afia@yahoo.com>
2023-12-04 14:47:39 -0800

    simple_shell project

--- lists.c
+++ lists.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * add_node - adds a node to the start of the list
+ * add_node - program adds a node to the start of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
- *
- * Return: size of list
+ * Return: Successful when size of list
  */
+
 list_t *add_node(list_t **head, const char *str, int num)
 {
 	list_t *new_head;
@@ -34,13 +34,13 @@ list_t *add_node(list_t **head, const char *str, int num)
 }
 
 /**
- * add_node_end - adds a node to the end of the list
+ * add_node_end - program adds a node to the end of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
- *
- * Return: size of list
+ * Return: Successful when size of list
  */
+
 list_t *add_node_end(list_t **head, const char *str, int num)
 {
 	list_t *new_node, *node;
@@ -75,36 +75,36 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
- * print_list_str - prints only the str element of a list_t linked list
+ * print_list_str - program prints only the str element of a list_t linked list
  * @h: pointer to first node
- *
- * Return: size of list
+ * Return: Successful size of list
  */
+
 size_t print_list_str(const list_t *h)
 {
-	size_t i = 0;
+	size_t j = 0;
 
 	while (h)
 	{
 		_puts(h->str ? h->str : "(nil)");
 		_puts("\n");
 		h = h->next;
-		i++;
+		j++;
 	}
-	return (i);
+	return (j);
 }
 
 /**
- * delete_node_at_index - deletes node at given index
+ * delete_node_at_index - program deletes node at given index
  * @head: address of pointer to first node
  * @index: index of node to delete
- *
- * Return: 1 on success, 0 on failure
+ * Return: Successful when 1 else 0 on failure
  */
+
 int delete_node_at_index(list_t **head, unsigned int index)
 {
 	list_t *node, *prev_node;
-	unsigned int i = 0;
+	unsigned int j = 0;
 
 	if (!head || !*head)
 		return (0);
@@ -120,14 +120,14 @@ int delete_node_at_index(list_t **head, unsigned int index)
 	node = *head;
 	while (node)
 	{
-		if (i == index)
+		if (j == index)
 		{
 			prev_node->next = node->next;
 			free(node->str);
 			free(node);
 			return (1);
 		}
-		i++;
+		j++;
 		prev_node = node;
 		node = node->next;
 	}
@@ -135,11 +135,11 @@ int delete_node_at_index(list_t **head, unsigned int index)
 }
 
 /**
- * free_list - frees all nodes of a list
+ * free_list - program frees all nodes of a list
  * @head_ptr: address of pointer to head node
- *
- * Return: void
+ * Return: Always successful when void
  */
+
 void free_list(list_t **head_ptr)
 {
 	list_t *node, *next_node, *head;
@@ -157,4 +157,3 @@ void free_list(list_t **head_ptr)
 	}
 	*head_ptr = NULL;
 }
-
