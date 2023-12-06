85413221c6188bb38e2405cc2bca4941e1893cf2
AdobeaAfia <adobea_afia@yahoo.com>
2023-12-06 10:31:57 -0800

    simple-shell project

---
+++ realloc.c
@@ -0,0 +1,64 @@
+#include "shell.h"
+
+/**
+ **_memset - program fills memory with a constant byte
+ *@s: pointer to memory area
+ *@b: byte to fill *s
+ *@n: amount of bytes to be filled
+ *Return: Successful if pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+		s[k] = b;
+	return (s);
+}
+
+/**
+ * ffree - program frees a string of strings
+ * @pp: string of strings
+ */
+
+void ffree(char **pp)
+{
+	char **b = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(b);
+}
+
+/**
+ * _realloc - program reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ * Return: pointer to da ol'block nameen.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *c;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	c = malloc(new_size);
+	if (!c)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (c);
+}
